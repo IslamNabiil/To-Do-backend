03ca876a026e7a0b3f007ff77cb21924e116bfc7
IslamNabiil <eslamoka1123@gmail.com>
2025-12-15 22:44:58 +0200

    Edit : taskModel.js : description default is empty and adding limit

--- models/TaskModel.js
+++ models/TaskModel.js
@@ -6,9 +6,13 @@ const taskSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
+      maxlength: [100, "Title cannot be more than 100 characters"],
     },
     description: {
       type: String,
+      default: "",
+      trim: true,
+      maxlength: [500, "Description cannot be more than 500 characters"],
     },
     isCompleted: {
       type: Boolean,
