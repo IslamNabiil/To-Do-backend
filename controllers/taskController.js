8795ad9856b34938e7f9fcf824784b04064b9e88
IslamNabiil <eslamoka1123@gmail.com>
2025-12-15 21:08:20 +0200

    Adding taskController.js file

---
+++ controllers/taskController.js
@@ -0,0 +1,26 @@
+const Task = require("../models/TaskModel");
+
+// Create a new task
+exports.createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    if (!title) {
+      return res.status(400).json({ message: "Title is required" });
+    }
+    const newTask = new Task({ title, description });
+    const savedTask = await newTask.save();
+    res.status(201).json(savedTask);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+// Get all tasks
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find().sort({ createdAt: -1 });
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
